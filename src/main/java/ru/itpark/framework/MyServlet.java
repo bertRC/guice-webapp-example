f3925c59f4d192987843d781fd2d95898a83d6bb
bertRC <sithalbert@ya.ru>
2019-12-07 16:22:32 +0300

    frontpage.jsp added

--- src/main/java/ru/itpark/framework/MyServlet.java
+++ src/main/java/ru/itpark/framework/MyServlet.java
@@ -3,6 +3,7 @@ package ru.itpark.framework;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,8 +19,9 @@ public class MyServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //Самая простая реализация без jsp-страничек
-        resp.getWriter().print(controller.printMessage("Hello world!!!"));
+        req.setAttribute("text", controller.printMessage("Hello world!!!"));
+        req.getRequestDispatcher("/WEB-INF/frontpage.jsp").forward(req, resp);
     }
 }
